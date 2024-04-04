57d8dfedae87c0db75bc61ad2b7695ade966a007
weiyi <xiaoyi001yeye@gmail.com>
2024-04-04 18:06:10 +0800

    增加java实现

---
+++ java_main/src/main/java/me/ve/smart/code/CodeQlRest.java
@@ -0,0 +1,16 @@
+package me.ve.smart.code.rest;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/codql")
+public class CodeQlRest {
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/welcome")
+    public String welcome() {
+        return "welcome to codeql java.";
+    }
+}
